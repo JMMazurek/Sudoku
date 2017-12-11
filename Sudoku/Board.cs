541b220330f82dae0cc29a3fff82a7f698f8d662
JMMazurek <jakubmmazurek@gmail.com>
2017-12-11 20:44:21 +0100

    Added files

---
+++ Sudoku/Board.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Sudoku
+{
+    public class Board
+    {
+        public ICommand ButtonCommand { get; set; }
+        public delegate void lockedDelegate(int n, bool locked);
+        public event lockedDelegate lockedChange = null;
+
+        public List<Cell> cells = new List<Cell>();
+
+        int selectedID = 0;
+
+        public Board()
+        {
+            ButtonCommand = new RelayCommand(
+                new Action<object>(delegate (object obj)
+                {
+                    string val = obj as string;
+                    cells[selectedID].Content = val;
+                    Check();
+                    if (Win())
+                        MessageBox.Show("Congratulations! You won!");
+                }));
+            for (int i = 0; i < 9*9; i++)
+            {
+                Cell cell = new Cell(i);
+                cell.Clicked += Clicked;
+                cells.Add(cell);
+            }
+            SetBoard();
+        }
+
+        public void Clicked(int cellID)
+        {
+            if (selectedID == cellID)
+                return;
+            if (cells[cellID].Locked)
+                return;
+            cells[selectedID].Selected = false;
+            selectedID = cellID;
+            cells[selectedID].Selected = true;
+
+            Check();
+        }
+        public void Check()
+        {
+            int col = selectedID % 9;
+            int row = selectedID / 9;
+            int sCol = col / 3;
+            int sRow = row / 3;
+            if (lockedChange == null)
+                return;
+            for (int i = 1; i < 10; i++)
+                lockedChange(i, false);
+
+            for (int i = 0; i < 9 * 9; i++)
+            {
+                if (i % 9 == col)
+                    lockedChange(cells[i].content, true);
+                if (i / 9 == row)
+                    lockedChange(cells[i].content, true);
+                if(i % 9 / 3 == sCol && i / 9 / 3 == sRow)
+                    lockedChange(cells[i].content, true);
+            }
+        }
+        public bool Win()
+        {
+            for (int i = 0; i < 9 * 9; i++)
+            {
+                if (cells[i].content == 0)
+                    return false;
+            }
+            return true;
+        }
+        public void SetBoard()
+        {
+            string[] arr=
+            {
+                "", "", "", "", "1", "", "8", "9", "",
+                "", "9", "8", "", "", "2", "7", "", "",
+                "7", "", "5", "3", "", "", "", "", "6",
+                "5", "4", "1", "2", "", "", "", "", "",
+                "", "", "", "", "", "", "", "", "",
+                "", "", "", "", "", "8", "6", "1", "4",
+                "4", "", "", "", "", "3", "1", "", "2",
+                "", "", "6", "9", "", "", "3", "5", "",
+                "", "7", "3", "", "2", "", "", "", ""
+            };
+            for (int i = 0; i < 9 * 9; i++)
+            {
+                cells[i].Content = arr[i];
+                cells[i].Locked = !(arr[i] == "");
+                cells[i].Selected = false;
+            }
+            selectedID = 0;
+            cells[0].Selected = true;
+            Check();
+        }
+    }
+
+    public class Cell : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = null;
+        public delegate void ClickedDelegate(int cellID);
+        public event ClickedDelegate Clicked = null;
+
+        int ID;
+        public int content = 0;
+        private bool locked;
+        private bool selected;
+
+        public Cell(int ID)
+        {
+            this.ID = ID;
+        }
+
+        public void Click(object sender, EventArgs e)
+        {
+            if (Clicked != null)
+                Clicked(ID);
+        }
+
+        public string Content
+        {
+            get
+            {
+                if (content != 0)
+                    return content.ToString();
+                else
+                    return "";
+            }
+            set
+            {
+                try
+                {
+                    content = int.Parse(value);
+                }
+                catch
+                {
+                    content = 0;
+                }
+                OnPropertyChanged("Content");
+            }
+        }
+
+        public bool Locked
+        {
+            get { return locked; }
+            set
+            {
+                locked = value;
+                OnPropertyChanged("Locked");
+            }
+        }
+        public bool Selected
+        {
+            get { return selected; }
+            set
+            {
+                selected = value;
+                OnPropertyChanged("Selected");
+            }
+        }
+        virtual protected void OnPropertyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
---
+++ Sudoku/MainWindow.xaml.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sudoku
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Board board;
+        public MainWindow()
+        {
+            InitializeComponent();
+            board = new Board();
+            tilesGrid.DataContext = board;
+            drawBoxes();
+            NewGame.Click += (s, e) => { board.SetBoard(); };
+            board.lockedChange += (n, locked) =>
+            {
+                switch (n)
+                {
+                    case 1: B1.IsEnabled = !locked; break;
+                    case 2: B2.IsEnabled = !locked; break;
+                    case 3: B3.IsEnabled = !locked; break;
+                    case 4: B4.IsEnabled = !locked; break;
+                    case 5: B5.IsEnabled = !locked; break;
+                    case 6: B6.IsEnabled = !locked; break;
+                    case 7: B7.IsEnabled = !locked; break;
+                    case 8: B8.IsEnabled = !locked; break;
+                    case 9: B9.IsEnabled = !locked; break;
+                    default: break;
+                }
+            };
+            board.Check();
+        }
+
+        public void drawBoxes()
+        {
+            for(int i = 0; i < 9; i++)
+                for(int j = 0; j < 9; j++)
+                {
+                    int tT = i % 3 == 0 ? 2 : 1;
+                    int bT = i % 3 == 2 ? 2 : 1;
+                    int lT = j % 3 == 0 ? 2 : 1;
+                    int rT = j % 3 == 2 ? 2 : 1;
+                    Label lab = new Label()
+                    {
+                        BorderThickness = new Thickness(lT, tT, rT, bT),
+                        BorderBrush = Brushes.Black,
+                        Content = (i * 10 + j).ToString(),
+                        VerticalContentAlignment = VerticalAlignment.Center,
+                        HorizontalContentAlignment = HorizontalAlignment.Center,
+                        FontStretch = FontStretches.Normal,
+                        FontSize = 16          
+                };
+                    Binding contentBbind = new Binding();
+                    contentBbind.Mode = BindingMode.Default;
+                    contentBbind.Source = board.cells;
+                    contentBbind.Path = new PropertyPath("["+ (i * 9 + j).ToString() + "].Content");
+                    lab.SetBinding(Label.ContentProperty, contentBbind);
+
+                    Binding selectedBind = new Binding();
+                    selectedBind.Converter = new BoolToBrushConverter();        
+                    selectedBind.Mode = BindingMode.Default;
+                    selectedBind.Source = board.cells;
+                    selectedBind.Path = new PropertyPath("[" + (i * 9 + j).ToString() + "].Selected");
+                    lab.SetBinding(Label.BackgroundProperty, selectedBind);
+
+                    Binding lockedBind = new Binding();
+                    lockedBind.Converter = new BoolToBrushConverter2();
+                    lockedBind.Mode = BindingMode.Default;
+                    lockedBind.Source = board.cells;
+                    lockedBind.Path = new PropertyPath("[" + (i * 9 + j).ToString() + "].Locked");
+                    lab.SetBinding(Label.ForegroundProperty, lockedBind);
+
+                    tilesGrid.Children.Add(lab);
+                    Grid.SetColumn(lab, j);
+                    Grid.SetRow(lab, i);
+                    lab.MouseDown += board.cells[i*9+j].Click;
+                }
+        }
+
+        private void MyGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+
+    public class BoolToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value)
+                return Brushes.LightGray;
+            return Brushes.Beige;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class BoolToBrushConverter2 : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value)
+                return Brushes.DarkGray;
+            return Brushes.Black;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
